9753ad04053113428510c0ed512c2de06e7db866
Philippe <Philippe@Philippe-Portable>
2016-10-25 15:10:42 -0400

    phillipe

--- src/projet2/hello.java
+++ src/projet2/hello.java
@@ -6,7 +6,11 @@ public class hello {
 		// TODO Auto-generated method stub
 		System.out.println("yoa in 2016");
 		System.out.println("::::»»»»»»»");
-		System.out.println("abcdefghijkmnopqrstuvwxyz");
+		System.out.println("4thuuy7yy88u88y7y44t6rt"
+				+ "t67t4ry74y7ry47yr"
+				+ "ru4y7rt43r737r43y47r"
+				+ "y437rt7tr7trr");
+		
 	}
 
 }
