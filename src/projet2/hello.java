8d03f517b167332294dae2c0cfc710dd9e21365e
Luxed <cocodu13820@hotmail.fr>
2016-10-25 13:42:27 -0400

    ksyygsiugtr

--- src/projet2/hello.java
+++ src/projet2/hello.java
@@ -4,7 +4,8 @@ public class hello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("wow in 201611111111111");
+		System.out.println("wow in 2016");
+		System.out.println("yoa in 2016");
 	}
 
 }
