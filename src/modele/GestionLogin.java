eb9cec2730ae62c09f718337da3a1eb695637a21
theguyovrther <theguyovrther123@yahoo.com>
2016-10-28 23:04:27 -0400

    gestion du login

---
+++ src/modele/GestionLogin.java
@@ -0,0 +1,94 @@
+package modele;
+
+import java.awt.Color;
+import java.awt.event.*;
+
+import javax.swing.*;
+
+import vue.identification;
+
+public class GestionLogin implements ActionListener, MouseListener{
+
+	private JTextField JTFnum;
+	private JPasswordField JPFpasse;
+	private JButton JBconnex;
+	private JCheckBox JCBsouv;
+	String nomSauvegarde = "";
+	private char[] e = {'p','a','s','s','w','o','r','d'}, e1 ={};
+
+	public GestionLogin(JTextField JTFnum,JPasswordField JPFpasse, JButton JBconnex,JCheckBox JCBsouv){
+		this.JTFnum = JTFnum;
+		this.JPFpasse = JPFpasse;
+		this.JBconnex = JBconnex;
+		this.JCBsouv = JCBsouv;
+	}
+
+
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		Object o = arg0.getSource();
+
+		if(o == JBconnex){
+			/*
+			Verifier le nom d'utilisateur et mot de passe
+			Est-il un utilisateur ou admin
+			*/
+			
+			if(JCBsouv.isSelected()){
+				nomSauvegarde = JTFnum.getText();
+			}
+		}
+
+		if(String.valueOf(JPFpasse.getPassword()).equals("")){
+			JPFpasse.setText("password");
+			JPFpasse.setForeground(Color.lightGray);
+		}
+		if(JTFnum.getText().equals("")){
+			JTFnum.setText("Nom d'utilisateur");
+			JTFnum.setForeground(Color.lightGray);
+		}
+	}
+
+
+	@Override
+	public void mouseClicked(MouseEvent arg0) {
+		Object o = arg0.getSource();
+
+		if(o == JTFnum && (JTFnum.getText().equals("Nom d'utilisateur") || JTFnum.getText().equals(""))){
+			JTFnum.setText("");
+			JTFnum.setForeground(Color.black);
+		}
+		if(o != JPFpasse && String.valueOf(JPFpasse.getPassword()).equals("")){
+			JPFpasse.setText("Mot de passe");
+			JPFpasse.setForeground(Color.lightGray);
+		}
+		if(o == JPFpasse && (JPFpasse.getPassword().equals("") || String.valueOf(JPFpasse.getPassword()).equals("Mot de passe"))){
+			JPFpasse.setText("");
+			JPFpasse.setForeground(Color.black);
+		}
+		if(o != JTFnum && JTFnum.getText().equals("")){
+			JTFnum.setText("Nom d'utilisateur");
+			JTFnum.setForeground(Color.lightGray);
+		}
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent arg0) {
+	}
+
+	@Override
+	public void mouseExited(MouseEvent arg0) {
+	}
+
+	@Override
+	public void mousePressed(MouseEvent arg0) {
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent arg0) {
+
+	}
+
+
+}
--- src/vue/identification.java
+++ src/vue/identification.java
@@ -1,74 +1,71 @@
 package vue;
 
-import java.awt.Color;
-import java.awt.Container;
-import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.GridLayout;
-import java.awt.Insets;
-
+import java.awt.*;
 import javax.swing.*;
 
-public class identification extends JFrame{
+import modele.GestionLogin;
 
-	JTextField JTFnumInscription,JTFmotpasse;
-	JLabel JLBnum,JLBmdpasse,JLBsouvenir;
-	JPanel panel1,panel2;
-	JButton connex,effacer;
-	JCheckBox JCBsouvenir;
-	Color couleur = new Color(103,204,201) ;
+public final class identification extends JFrame{
 	
-	identification(){
+	private static identification instance;
+	JTextField JTFnumInscription;
+	JPasswordField JPFmotpasse;
+	JLabel JLBnum,JLBmdpasse,JLBsouvenir;
+	JButton connex;
+	JCheckBox JCBsouvenir;
+	GridBagConstraints gbl = new GridBagConstraints();
+	
+	
+	
+	private identification(){
 		
 		super("Authentification");
-		setLayout(null);
-		GridBagConstraints gbl = new GridBagConstraints();
-		Container c = getContentPane();
+		
+		
 		JLBnum = new JLabel("");
 		JLBmdpasse = new JLabel("");
 		JLBsouvenir = new JLabel(" se souvenir de moi?");
 		JTFnumInscription = new JTextField("Nom d'utilisateur");
-		JTFmotpasse = new JTextField("Mot de passe");
-		panel1 = new JPanel();
+		JPFmotpasse = new JPasswordField("Mot de passe");
 		connex = new JButton("Connexion");
 		JCBsouvenir = new JCheckBox();
 		setLayout(new GridBagLayout());
-		effacer = new JButton("Effacer");
 		
 		JLBnum.setIcon(new ImageIcon("usericon.png"));
 		JLBmdpasse.setIcon(new ImageIcon("password-icon.png"));
 		
-		JTFmotpasse.setBorder(BorderFactory.createEmptyBorder());
-		JTFnumInscription.setBorder(BorderFactory.createEmptyBorder());
+		JPFmotpasse.setBorder(BorderFactory.createEmptyBorder(0,3,0,0));
+		JTFnumInscription.setBorder(BorderFactory.createEmptyBorder(0,3,0,0));
 		
 		JTFnumInscription.setFont(new Font("Arial",Font.PLAIN,15));
-		JTFmotpasse.setFont(new Font("Arial",Font.PLAIN,15));
+		JPFmotpasse.setFont(new Font("Arial",Font.PLAIN,15));
 		JLBsouvenir.setFont(new Font("Arial",Font.PLAIN,15));
 		JTFnumInscription.setForeground(Color.lightGray);
-		JTFmotpasse.setForeground(Color.lightGray);
-		
+		JPFmotpasse.setForeground(Color.lightGray);
 		
 		gbl.gridx = 0;
 		gbl.gridy = 0;
 		gbl.insets = new Insets(9,0,0,0);
 		add(JLBnum,gbl);
+		
 		gbl.insets = new Insets(0,0,0,0);
 		gbl.gridx = 0;
 		gbl.gridy = 1;
 		add(JLBmdpasse,gbl);
+		
 		gbl.insets = new Insets(9,0,0,0);
 		gbl.gridwidth = 1;
 		gbl.ipady = 16;
 		gbl.gridx = 1;
 		gbl.gridy = 0;
 		add(JTFnumInscription,gbl);
+		
 		gbl.insets = new Insets(0,0,0,0);
 		gbl.anchor = GridBagConstraints.CENTER;
 		gbl.gridwidth = 1;
 		gbl.gridx = 1;
 		gbl.gridy = 1;
-		add(JTFmotpasse,gbl);
+		add(JPFmotpasse,gbl);
 		
 		gbl.gridx = 0;
 		gbl.gridy = 2;
@@ -89,9 +86,14 @@ public class identification extends JFrame{
 		gbl.anchor = GridBagConstraints.PAGE_END;
 		gbl.insets = new Insets(0,100,8,0);
 		add(connex,gbl);
+		GestionLogin g = new GestionLogin(JTFnumInscription, JPFmotpasse, connex, JCBsouvenir);
+		JTFnumInscription.addMouseListener(g);
+		JPFmotpasse.addMouseListener(g);
+		connex.addActionListener(g);
+		JCBsouvenir.addActionListener(g);
 		
 		JTFnumInscription.setColumns(15);
-		JTFmotpasse.setColumns(15);
+		JPFmotpasse.setColumns(15);
 		setSize(270,180);
 		setVisible(true);
 		setLocationRelativeTo(null);
@@ -100,8 +102,15 @@ public class identification extends JFrame{
 	
 	
 	public static void main(String[] args) {
-		identification identi = new identification();
+		identification identi = identification.getInstance();
 		identi.setDefaultCloseOperation(EXIT_ON_CLOSE);
+	}
+
+
+	public static identification getInstance() {
+		if(instance == null)
+			instance = new identification();
+		return instance;
 	}
 
 }
